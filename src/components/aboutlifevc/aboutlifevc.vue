e2c482632946264c4bf6de0bf99ad61776d64726
YuemT <yuem_t@163.com>
2017-07-13 07:55:23 +0800

    完成静态页面和基本页面跳转

--- src/components/aboutlifevc/aboutlifevc.vue
+++ src/components/aboutlifevc/aboutlifevc.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="aboutlifevc">
-      <div class="aboutlifevc-header">
-        <router-link to="/">
-          <img src="./back-icon.png" alt="" >
-        </router-link>
-        了解LifeVC
+    <transition name="aboutlifevc">
+      <div class="aboutlifevc">
+        <div class="aboutlifevc-header">
+          <router-link to="/">
+            <img src="./back-icon.png" alt="" >
+          </router-link>
+          了解LifeVC
+        </div>
+        <div class="aboutlifevc-content">
+          <aboutdetail></aboutdetail>
+          <aboutdetail></aboutdetail>
+          <aboutdetail></aboutdetail>
+          <aboutdetail></aboutdetail>
+          <aboutdetail></aboutdetail>
+        </div>
       </div>
-      <div class="aboutlifevc-content">
-        <aboutdetail></aboutdetail>
-        <aboutdetail></aboutdetail>
-        <aboutdetail></aboutdetail>
-        <aboutdetail></aboutdetail>
-        <aboutdetail></aboutdetail>
-      </div>
-    </div>
+    </transition>
 </template>
 
 <script>
@@ -26,26 +28,33 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-.aboutlifevc
-  width 100%
-  min-height 100%
-  background-color white
-  position absolute
-  z-index 100
-  .aboutlifevc-header
-    height 48px
+  .aboutlifevc-enter-active, .aboutlifevc-leave-active
+    transition: all .2s linear
+
+  .aboutlifevc-enter, .aboutlifevc-leave-to
+    transform translateX(375px)
+  .aboutlifevc
     width 100%
-    background-color #89be48
-    color white
-    font-size 20px
-    text-align center
-    line-height 48px
-    img
-      position absolute
-      left 12px
-      top 14px
-      width 12px
-  .aboutlifevc-content
-    width 92%
-    margin 0 auto
+    min-height 100%
+    background-color white
+    position absolute
+    z-index 100
+
+    .aboutlifevc-header
+      position fixed
+      height 48px
+      width 100%
+      background-color #89be48
+      color white
+      font-size 20px
+      text-align center
+      line-height 48px
+      img
+        position absolute
+        left 12px
+        top 14px
+        width 12px
+    .aboutlifevc-content
+      width 92%
+      margin 48px auto 0
 </style>
